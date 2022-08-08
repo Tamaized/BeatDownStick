b21e91174466acae0c12a59b249b0ef30ea66f71
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-08-08 07:40:02 -0600

    1.19 port (#3)

--- src/main/java/tamaized/beatdownstick/BeatDownStick.java
+++ src/main/java/tamaized/beatdownstick/BeatDownStick.java
@@ -1,28 +1,19 @@
 package tamaized.beatdownstick;
 
 
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootEntryItem;
-import net.minecraft.world.storage.loot.LootPool;
-import net.minecraft.world.storage.loot.LootTableList;
-import net.minecraft.world.storage.loot.conditions.LootCondition;
-import net.minecraft.world.storage.loot.functions.LootFunction;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.LootTableLoadEvent;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import com.mojang.serialization.Codec;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistryEntry;
-import net.minecraftforge.registries.ObjectHolder;
-import tamaized.beatdownstick.common.damagesource.DamageSourceAnnihilate;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import tamaized.beatdownstick.common.items.ItemBeatDownStick;
 import tamaized.beatdownstick.common.items.ItemSuperBeatDownStick;
-
-import java.util.Objects;
+import tamaized.beatdownstick.common.loot.BeatDownStickModifier;
 
 @Mod(BeatDownStick.MODID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -30,60 +21,19 @@ public class BeatDownStick {
 
 	public static final String MODID = "beatdownstick";
 
-	public static final DamageSource DAMAGE_SOURCE_ANNIHILATE = new DamageSourceAnnihilate(BeatDownStick.MODID + ".annihilate").setDamageBypassesArmor().setDamageAllowedInCreativeMode();
+	public static final DamageSource DAMAGE_SOURCE_ANNIHILATE = new DamageSource(BeatDownStick.MODID + ".annihilate").bypassArmor().bypassInvul();
 
+	public static final DeferredRegister<Item> ITEM_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+	public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIER_REGISTRY = DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, MODID);
 
-	@ObjectHolder("beatdownstick")
-	public static final Item beatDownStick = Items.AIR;
-	@ObjectHolder("superbeatdownstick")
-	public static final Item superBeatDownStick = Items.AIR;
+	public static final RegistryObject<Item> BEAT_DOWN_STICK = ITEM_REGISTRY.register("beatdownstick", () -> new ItemBeatDownStick(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1).durability(21)));
+	public static final RegistryObject<Item> SUPER_BEAT_DOWN_STICK = ITEM_REGISTRY.register("superbeatdownstick", () -> new ItemSuperBeatDownStick(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1)));
+
+	public static final RegistryObject<Codec<BeatDownStickModifier>> BEAT_DOWN_STICK_LOOT_INJECTION = LOOT_MODIFIER_REGISTRY.register("loot_injection", () -> BeatDownStickModifier.CODEC);
 
 	public BeatDownStick() {
-		MinecraftForge.EVENT_BUS.addListener(this::modifyLootTables);
-	}
-
-	public static ResourceLocation getRegName(ForgeRegistryEntry entry) {
-		return Objects.requireNonNull(entry.getRegistryName());
-	}
-
-	@SubscribeEvent
-	public static void registerItems(RegistryEvent.Register<Item> event) {
-		event.getRegistry().registerAll(
-
-				assign(new ItemBeatDownStick(ItemGroup.COMBAT), "beatdownstick"),
-
-				assign(new ItemSuperBeatDownStick(ItemGroup.COMBAT), "superbeatdownstick")
-
-		);
-	}
-
-	private static Item assign(Item item, String name) {
-		return item
-
-				.setRegistryName(MODID, name);
-	}
-
-	private void modifyLootTables(LootTableLoadEvent e) {
-		if (e.getTable() == null)
-			return;
-		LootPool pool = e.getTable().getPool("main");
-		//noinspection ConstantConditions; getPool can return null
-		if (pool == null)
-			return;
-
-		if (e.getName().equals(LootTableList.CHESTS_SIMPLE_DUNGEON) ||
-
-				e.getName().equals(LootTableList.CHESTS_JUNGLE_TEMPLE) ||
-
-				e.getName().equals(LootTableList.CHESTS_DESERT_PYRAMID) ||
-
-				e.getName().equals(LootTableList.CHESTS_STRONGHOLD_CORRIDOR) ||
-
-				e.getName().equals(LootTableList.CHESTS_STRONGHOLD_CROSSING) ||
-
-				e.getName().equals(LootTableList.CHESTS_STRONGHOLD_LIBRARY)) {
-			pool.addEntry(new LootEntryItem(beatDownStick, 1, 0, new LootFunction[0], new LootCondition[0], getRegName(beatDownStick).toString()));
-		}
+		ITEM_REGISTRY.register(FMLJavaModLoadingContext.get().getModEventBus());
+		LOOT_MODIFIER_REGISTRY.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 
 }
