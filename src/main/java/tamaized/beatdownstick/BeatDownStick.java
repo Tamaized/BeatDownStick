1681a54df43f4629e85df4337c3e50260756e412
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-09-09 09:50:52 -0600

    Beat Down Stick changes, fixes, and improvements (#4)

--- src/main/java/tamaized/beatdownstick/BeatDownStick.java
+++ src/main/java/tamaized/beatdownstick/BeatDownStick.java
@@ -1,8 +1,12 @@
 package tamaized.beatdownstick;
 
-
 import com.mojang.serialization.Codec;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
@@ -12,7 +16,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import tamaized.beatdownstick.common.items.ItemBeatDownStick;
-import tamaized.beatdownstick.common.items.ItemSuperBeatDownStick;
 import tamaized.beatdownstick.common.loot.BeatDownStickModifier;
 
 @Mod(BeatDownStick.MODID)
@@ -23,17 +26,23 @@ public class BeatDownStick {
 
 	public static final DamageSource DAMAGE_SOURCE_ANNIHILATE = new DamageSource(BeatDownStick.MODID + ".annihilate").bypassArmor().bypassInvul();
 
+	public static final TagKey<EntityType<?>> DONT_ONE_SHOT = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation(MODID, "stick_doesnt_one_shot"));
+
 	public static final DeferredRegister<Item> ITEM_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 	public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIER_REGISTRY = DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, MODID);
+	public static final DeferredRegister<SoundEvent> SOUND_REGISTRY = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
 
-	public static final RegistryObject<Item> BEAT_DOWN_STICK = ITEM_REGISTRY.register("beatdownstick", () -> new ItemBeatDownStick(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1).durability(21)));
-	public static final RegistryObject<Item> SUPER_BEAT_DOWN_STICK = ITEM_REGISTRY.register("superbeatdownstick", () -> new ItemSuperBeatDownStick(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1)));
+	public static final RegistryObject<Item> BEAT_DOWN_STICK = ITEM_REGISTRY.register("beatdownstick", () -> new ItemBeatDownStick(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1).durability(21), false));
+	public static final RegistryObject<Item> SUPER_BEAT_DOWN_STICK = ITEM_REGISTRY.register("superbeatdownstick", () -> new ItemBeatDownStick(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1), true));
 
 	public static final RegistryObject<Codec<BeatDownStickModifier>> BEAT_DOWN_STICK_LOOT_INJECTION = LOOT_MODIFIER_REGISTRY.register("loot_injection", () -> BeatDownStickModifier.CODEC);
+
+	public static final RegistryObject<SoundEvent> WHAM = SOUND_REGISTRY.register("item.beatdownstick.wham", () -> new SoundEvent(new ResourceLocation(MODID, "item.beatdownstick.wham")));
 
 	public BeatDownStick() {
 		ITEM_REGISTRY.register(FMLJavaModLoadingContext.get().getModEventBus());
 		LOOT_MODIFIER_REGISTRY.register(FMLJavaModLoadingContext.get().getModEventBus());
+		SOUND_REGISTRY.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 
 }
--- src/main/java/tamaized/beatdownstick/common/items/ItemBeatDownStick.java
+++ src/main/java/tamaized/beatdownstick/common/items/ItemBeatDownStick.java
@@ -1,5 +1,7 @@
 package tamaized.beatdownstick.common.items;
 
+import net.minecraft.network.protocol.game.ClientboundAnimatePacket;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -7,36 +9,70 @@ import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.entity.PartEntity;
+import tamaized.beatdownstick.BeatDownStick;
 
 public class ItemBeatDownStick extends Item {
 
-	public ItemBeatDownStick(Properties properties) {
+	private final boolean superStick;
+
+	public ItemBeatDownStick(Properties properties, boolean superStick) {
 		super(properties);
+		this.superStick = superStick;
 	}
 
 	@Override
 	public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
 		boolean flag = false;
+		float dmg;
+		boolean dontOneShot = entity.getType().is(BeatDownStick.DONT_ONE_SHOT);
+		DamageSource source = this.superStick ? BeatDownStick.DAMAGE_SOURCE_ANNIHILATE : DamageSource.GENERIC;
 		if (entity instanceof LivingEntity living) {
-			float dmg;
-			dmg = (!living.canChangeDimensions()) ? (living.getMaxHealth() / 10) : (living.getMaxHealth()); // do 10% instead of 100% dmg to bosses
-			living.hurt(DamageSource.GENERIC, dmg);
-			flag = true;
+			dmg = this.superStick ? Float.MAX_VALUE : dontOneShot ? (living.getMaxHealth() / 10) : (living.getMaxHealth()); // do 10% instead of 100% dmg to bosses
+			player.playSound(BeatDownStick.WHAM.get(), 1.0F, 1.0F);
+			if (living.hurt(source, dmg))
+				flag = true;
 		} else if (entity instanceof PartEntity<?> part) {
-			if (part.getParent() instanceof EnderDragon) // Must be DamageSource.Player for dragon
-				part.hurt(DamageSource.playerAttack(player), 20);
-			else if (part.getParent() instanceof LivingEntity living) {
-				living.hurt(DamageSource.GENERIC, (!part.canChangeDimensions()) ? (living.getMaxHealth() / 10) : (living.getMaxHealth()));
+			if (part.getParent() instanceof EnderDragon dragon) { // Must be DamageSource.Player for dragon
+				player.playSound(BeatDownStick.WHAM.get(), 1.0F, 1.0F);
+				if (part.hurt(DamageSource.playerAttack(player), this.superStick ? Float.MAX_VALUE : (dragon.getMaxHealth() / 10)))
+					flag = true;
+			} else if (part.getParent() instanceof LivingEntity living) {
+				dmg = this.superStick ? Float.MAX_VALUE : dontOneShot ? (living.getMaxHealth() / 10) : (living.getMaxHealth());
+				player.playSound(BeatDownStick.WHAM.get(), 1.0F, 1.0F);
+				if (living.hurt(source, dmg))
+					flag = true;
 			}
-			flag = true;
 		}
 		if (flag) {
-			if (player.getAbilities().instabuild)
-				return true;
-			stack.hurtAndBreak(1, player, user -> user.broadcastBreakEvent(player.swingingArm));
+			if (!player.getAbilities().instabuild && !this.superStick)
+				stack.hurtAndBreak(1, player, user -> user.broadcastBreakEvent(player.swingingArm));
+			if (player.getLevel() instanceof ServerLevel server) {
+				server.getChunkSource().broadcastAndSend(player, new ClientboundAnimatePacket(entity, 5));
+			}
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public boolean isEnchantable(ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+		return false;
+	}
+
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+		return false;
+	}
+
+	@Override
+	public float getXpRepairRatio(ItemStack stack) {
+		return 0.0F;
 	}
 }
