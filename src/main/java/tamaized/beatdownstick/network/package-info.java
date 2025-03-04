05d6ebe8dafc9db1d66b25ec3f4d8e493f3f7516
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-03-03 23:47:30 -0500

    Fix update.json

---
+++ src/main/java/tamaized/beatdownstick/network/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package tamaized.beatdownstick.network;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
