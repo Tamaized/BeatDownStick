ad9b05a885f67973912d55c35688c5a1e7c9efcf
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-05-24 16:25:51 -0400

    Update package-info files

--- src/data/java/tamaized/beatdownstick/datagen/lang/package-info.java
+++ src/data/java/tamaized/beatdownstick/datagen/lang/package-info.java
@@ -2,7 +2,7 @@
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 @TypeUseAreNonnullByDefault
-package tamaized.beatdownstick.common;
+package tamaized.beatdownstick.datagen.lang;
 
 import com.mojang.logging.annotations.FieldsAreNonnullByDefault;
 import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
